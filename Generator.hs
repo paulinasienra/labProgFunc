136b4660d34351073ea0d2ba98dd990f422b7cdd
Paulina Sienra Budnik <paulina.sienra@fing.edu.uy>
2022-06-16 20:59:55 -0300

    interpreter y algunos arreglos generator

--- Generator.hs
+++ Generator.hs
@@ -16,8 +16,9 @@ genCodMaq (Decl _:xs) = genCodMaq xs
 genCodMaq (Com stmt:xs) = fst (stmtCodMaq stmt) ++ genCodMaq xs
 
 stmtCodMaq :: Stmt -> (Code, Integer)
+stmtCodMaq (StmtExpr (Assign n e)) = (fst (exprCodMaq e) ++ [STORE n], snd (exprCodMaq e) + 1)
 stmtCodMaq (StmtExpr e) = exprCodMaq e
-stmtCodMaq (PutChar e) = exprCodMaq e -- 
+stmtCodMaq (PutChar e) = (fst (exprCodMaq e) ++ [WRITE], snd (exprCodMaq e) + 1) -- 
 stmtCodMaq (If e b1 b2) = (fst (exprCodMaq e) ++ JMPZ (fromIntegral(snd(bodyCodMaq b1))+ 1): fst(bodyCodMaq b1) ++ JUMP (fromIntegral(snd(bodyCodMaq b2))) : fst(bodyCodMaq b2), snd(bodyCodMaq b1) + snd(bodyCodMaq b2) + 2) -- evaluar expr
 stmtCodMaq (While e b) = (fst (exprCodMaq e) ++ JMPZ (fromIntegral(snd(bodyCodMaq b))+ 1) :fst(bodyCodMaq b) ++ [JUMP (-(fromIntegral(snd(exprCodMaq e)) + fromIntegral(snd(bodyCodMaq b))))], snd(bodyCodMaq b) + snd(exprCodMaq e) + 2) -- lo mismo
 
@@ -31,7 +32,7 @@ exprCodMaq (Var n) = ([LOAD n],1)
 exprCodMaq (CharLit c) = ([PUSH (toInteger$fromEnum c)],1) -- ASCII de c, ponele
 exprCodMaq (NatLit n) = ([PUSH n],1)
 exprCodMaq GetChar = ([READ],1)
-exprCodMaq (Assign n e) = (fst (exprCodMaq e) ++ [STORE n], snd (exprCodMaq e) + 1)
+exprCodMaq (Assign n e) = (fst (exprCodMaq e) ++ [STORE n]++[LOAD n], snd (exprCodMaq e) + 2)
 
 
 -- Aritmetica
@@ -43,11 +44,11 @@ exprCodMaq (Binary Mod e1 e2) = (fst(exprCodMaq e2) ++ fst(exprCodMaq e1) ++ [MO
 exprCodMaq (Unary Neg e) = (fst(exprCodMaq e) ++ [NEG], snd (exprCodMaq e) + 1)
 
 -- Logicos
-exprCodMaq (Binary Less e1 e2) = ( fst (exprCodMaq e2) ++ fst(exprCodMaq e1) ++ CMP: PUSH 1: ADD: JMPZ 3: PUSH 0: JUMP 1: [PUSH 1],snd (exprCodMaq e1) + snd (exprCodMaq e2) + 7)
-exprCodMaq (Binary Equ e1 e2) = ( fst (exprCodMaq e2) ++ fst(exprCodMaq e1) ++ CMP: JMPZ 3: PUSH 0: JUMP 1: [PUSH 1],snd (exprCodMaq e1) + snd (exprCodMaq e2) + 5)
+exprCodMaq (Binary Less e1 e2) = ( fst (exprCodMaq e2) ++ fst(exprCodMaq e1) ++ CMP: PUSH 1: ADD: JMPZ 2: PUSH 0: JUMP 1: [PUSH 1],snd (exprCodMaq e1) + snd (exprCodMaq e2) + 7)
+exprCodMaq (Binary Equ e1 e2) = ( fst (exprCodMaq e2) ++ fst(exprCodMaq e1) ++ CMP: JMPZ 2: PUSH 0: JUMP 1: [PUSH 1],snd (exprCodMaq e1) + snd (exprCodMaq e2) + 5)
 exprCodMaq (Unary Not e) = (fst (exprCodMaq e) ++ JMPZ 3: PUSH 0: JUMP 1: [PUSH 1], snd (exprCodMaq e) + 4)
-exprCodMaq (Binary And e1 e2) = ( fst (exprCodMaq e2) ++ fst(exprCodMaq e1) ++ ADD: PUSH 2: CMP:JMPZ 3:PUSH 0:JUMP 1:[PUSH 1] ,snd (exprCodMaq e1) + snd (exprCodMaq e2) + 7)
-exprCodMaq (Binary Or e1 e2) = ( fst (exprCodMaq e2) ++ fst(exprCodMaq e1) ++ ADD: JMPZ 3: PUSH 1: JUMP 1: [PUSH 0],snd (exprCodMaq e1) + snd (exprCodMaq e2) + 5)
+exprCodMaq (Binary And e1 e2) = ( fst (exprCodMaq e2) ++ fst(exprCodMaq e1) ++ ADD: PUSH 2: CMP:JMPZ 2:PUSH 0:JUMP 1:[PUSH 1] ,snd (exprCodMaq e1) + snd (exprCodMaq e2) + 7)
+exprCodMaq (Binary Or e1 e2) = ( fst (exprCodMaq e2) ++ fst(exprCodMaq e1) ++ ADD: JMPZ 2: PUSH 1: JUMP 1: [PUSH 0],snd (exprCodMaq e1) + snd (exprCodMaq e2) + 5)
 
 
 desparseoGen :: Either String Program -> Code
--- Interpreter.hs
+++ Interpreter.hs
@@ -4,6 +4,7 @@
 module Interpreter where
 
 import MachineLang
+import Data.Char (chr, ord)
 
 type Conf = (Stack,Env)
 
@@ -12,4 +13,37 @@ type Stack = [Integer]
 
 -- Implementar
 interp :: Code -> Code -> Conf -> IO Conf
-interp = undefined
+interp _ [SKIP] conf = return conf -- caso base, termino el codigo
+interp pc (SKIP:xs) conf = interp (SKIP:pc) xs conf
+interp pc (ADD:xs) (n:f:ys,amb) = interp (ADD:pc) xs ((n + f):ys,amb)
+interp pc (SUB:xs) (n:f:ys,amb) = interp (SUB:pc) xs ((n - f):ys,amb)
+interp pc (MUL:xs) (n:f:ys,amb) = interp (MUL:pc) xs ((n * f):ys,amb)
+interp pc (DIV:xs) (n:f:ys,amb) = interp (DIV:pc) xs (div n f:ys,amb)
+interp pc (MOD:xs) (n:f:ys,amb) = interp (MOD:pc) xs (mod n f:ys,amb)
+interp pc (NEG:xs) (n:ys,amb) = interp (NEG:pc) xs (-n:ys,amb)
+interp pc (CMP:xs) (n:f:ys,amb) | n == f = interp (CMP:pc) xs (0:ys,amb)
+                                | n < f = interp (CMP:pc) xs (1:ys,amb)
+                                | otherwise = interp (CMP:pc) xs (-1:ys,amb)
+interp pc (PUSH n:xs) (ys,amb) = interp (PUSH n:pc) xs (n:ys,amb)
+interp pc (JMPZ n:xs) (m:ys,amb) | m == 0 && n > 0 = interp (reverse (take n xs) ++ JMPZ n:pc) (drop n xs) (ys,amb)
+                                 | m == 0 = interp (drop n pc) (reverse (take n pc)++ JMPZ n:xs) (ys,amb)
+                                 | otherwise = interp (JMPZ n:pc) xs (ys,amb)
+interp pc (JUMP n:xs) (ys,amb) | n > 0 = interp (reverse (take n xs) ++ JUMP n:pc) (drop n xs) (ys,amb) -- Jump puede ser negativo
+                                | otherwise  = interp (drop n pc) (reverse (take n pc)++ JUMP n:xs) (ys,amb)
+interp pc (WRITE: xs) (n:ys,amb) = do putStr [chr (fromIntegral  n)];
+                                      interp (WRITE:pc) xs (ys,amb)
+interp pc (READ:xs) (ys,amb) = do line <- getChar;
+                                  interp (READ:pc) xs (toInteger(ord line):ys,amb)
+interp pc (STORE s:xs) (n:ys,amb) = interp (STORE s:pc) xs (ys,updVar (s,n) amb)
+interp pc (LOAD s:xs) (ys,amb) = interp (LOAD s:pc) xs (buscarValor s amb:ys,amb)
+
+
+buscarValor :: Var -> Env -> Integer
+buscarValor a (x:amb) | a ==fst x = snd x
+                      | otherwise = buscarValor a amb
+
+
+updVar :: (Var,Integer) -> Env -> Env
+updVar a [] = [a]
+updVar a (x:amb) | fst a ==fst x = updVar a amb
+                                | otherwise = x:updVar a amb
